9c2ba028f8c96f123ead4e2579d361bd133297a3
patu955 <simone.patuelli@icloud.com>
2022-12-20 11:57:16 +0100

    Update code to introduce Mongo support

---
+++ src/main/java/org/acme/mongodb/FruitResource.java
@@ -0,0 +1,26 @@
+package org.acme.mongodb;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import java.util.List;
+
+@Path("/fruits")
+public class FruitResource {
+
+    @Inject
+    FruitService fruitService;
+
+    @GET
+    public List<Fruit> list() {
+        return fruitService.list();
+    }
+
+    @POST
+    public List<Fruit> add(Fruit fruit) {
+        fruitService.add(fruit);
+        return list();
+    }
+}
